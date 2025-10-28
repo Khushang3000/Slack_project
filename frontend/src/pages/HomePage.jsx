3a31c8a3d47026bbf0270ad7cb1de49ad231a765
Khushang3000 <sharmakhushang05@gmail.com>
2025-10-28 16:28:05 +0530

    added tailwind, useStreamChat hook, and fixed app file

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='bg-red-300'>
         {/* userbutton to signout */}
         <UserButton/>
         Home page.    
