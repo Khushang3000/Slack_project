302a4a067488391ea2af55b067a686a0d2f58664
Khushang3000 <sharmakhushang05@gmail.com>
2025-10-29 14:53:39 +0530

    homepage channel form setup completed

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,8 +1,10 @@
 export const protectRoute = (req, res, next) => {
     if(!req.auth().isAuthenticated){
         //if user is not authenticated(this is not stream authentication, but clerk authentication)
+        console.log("route is protected")
         return res.status(401).json({message: "Unauthorized - you must be logged in"})
     }
 
+    next();
     // if user is authenticated then just call the next()
 }
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,17 @@
 import { axiosInstance } from "./axios";
 
 export async function getStreamToken(){
+    
     //remember we have an api endpoint which gives us a streamToken, in /chat/token, baseUrl we set in axios.js. axiosInstance.
-    const response = await axiosInstance.get("/chat/token");//if this was a post req or delete req then we woulda done .post or .delete.
-    return response.data;
+    try {
+        // console.log("getStreamToken frontend called")
+        const response = await axiosInstance.get("/chat/token");
+        // console.log("request to backend sent");
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching stream token:", error.message);
+        console.error("Full error object:", error);
+    }
+    
+    
 }
